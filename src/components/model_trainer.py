81d172f47a7133aaadd8f042ea89271e16b799a2
amitbharti017 <amitbharti017@gmail.com>
2024-03-10 20:01:06 +0100

    lightgbm added

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -10,6 +10,8 @@ from sklearn.metrics import root_mean_squared_error
 from xgboost import XGBRegressor
 from sklearn.model_selection import cross_val_score
 from optuna.integration.mlflow import MLflowCallback
+from utils.common import get_mlflow_experiment
+from utils.common import create_mlflow_experiment
 
 
 class ModelTrainer:
@@ -20,45 +22,56 @@ class ModelTrainer:
         X_train = joblib.load(self.config.data_X_train_path)
         y_train = joblib.load(self.config.data_y_train_path)
 
-        params = {
-            "booster": trial.suggest_categorical("booster",["gbtree","dart"]),
-            # "device" : "cuda",
-            "lambda": trial.suggest_float("lambda", 0.05,1.0),
-            "alpha": trial.suggest_float("alpha",0.05,1.0),
-            "objective": "reg:squarederror",
-            "eval_metric": "rmse",
-            "max_depth": trial.suggest_int("max_depth",3,14),
-            "n_estimators": trial.suggest_int("n_estimators",100,1000),
-            "subsample": trial.suggest_float("subsample",0.5,1.0),
-            "colsample_bytree": trial.suggest_float("colsample_bytree",0.5,1.0),
-            "gamma": trial.suggest_int("gamma",0,60),
-            "min_child_weight": trial.suggest_float("min_child_weight",1,10)
-        }
-        # params = {
-        #     'device': "cuda",
-        #     'tree_method': 'hist',
-        #     'objective':'reg:squarederror',
-        #     'learning_rate': trial.suggest_categorical('learning_rate', [0.008, 0.01, 0.02, 0.05]),
-        #     'random_state': 7,
-        #     'max_depth': trial.suggest_int('max_depth', 3, 10), #change
-        #     'min_child_weight': trial.suggest_int('min_child_weight', 1, 5), #change
-        #     'lambda': trial.suggest_float('lambda', 1e-4, 10.0, log=True),
-        #     'alpha': trial.suggest_float('alpha', 1e-4, 10.0, log=True),
-        # }
-
-        model = XGBRegressor(**params)
-
+        regressor_name = trail.suggest_categorical('regressor',['XGBoost',"LightGBM"])
+        if regressor_name == "XGBoost":
+            params_xgboost = {
+                "booster": trial.suggest_categorical("booster",["gbtree","dart"]),
+                "device" : "cuda",
+                "lambda": trial.suggest_float("lambda", 0.05,1.0),
+                "alpha": trial.suggest_float("alpha",0.05,1.0),
+                "objective": "reg:squarederror",
+                "eval_metric": "rmse",
+                "max_depth": trial.suggest_int("max_depth",3,10),
+                "n_estimators": trial.suggest_int("n_estimators",100,1000),
+                "subsample": trial.suggest_float("subsample",0.5,1.0),
+                "colsample_bytree": trial.suggest_float("colsample_bytree",0.5,1.0),
+                "gamma": trial.suggest_int("gamma",0,60),
+                "min_child_weight": trial.suggest_float("min_child_weight",1,10)
+            }
+            model = XGBRegressor(**params_xgboost)
+        else:
+            params_lightgbm = {
+                "num_leaves": trial.suggest_int("num_leaves",30,127),
+                "max_depth": trial.suggest_int("max_depth",3,10),
+                "min_data_in_leaf": trial.suggest_int("min_data_in_leaf",5,15),
+                "bagging_freq": trial.suggest_int("bagging_freq",4,8),
+                "bagging_fraction": trial.suggest_float("bagging_fraction",0.6,0.9),
+                "max_bin": trial.suggest_int("max_bin",150,250),
+                "lambda_l1": trial.suggest.float("lambda_l1",0.1,0.3),
+                "lambda_l2": trial.suggest.float("lambda_l2",0.1,0.3),
+                "min_sum_hessian_in_leaf": trial.suggest.float("min_sum_hessian_in_leaf",1e-4,1e-2),
+                "feature_fraction": trial.suggest.float("feature_fraction",0.6,0.9),
+                "learning_rate": trial.suggest.float("learning_rate",0.05,0.2),
+                "num_iterations": trial.suggest.int("num_iterations",70,130),
+                "early_stopping_rounds": trial.suggest.int("early_stopping_rounds",30,50)
+            }
+            model = LightGBM(**params_lightgbm)
         scores = cross_val_score(model,X_train,y_train,n_jobs=-1,cv=5,scoring="neg_root_mean_squared_error")
         rmse = -np.mean(scores)
-        mlflow.log_params(params)
-        mlflow.log_metric("rmse",rmse)
-        mlflow.end_run()
+        # with mlflow.start_run():
+        #     mlflow.log_params(params)
+        #     mlflow.log_metric("rmse",rmse)
     
-    def optimize_xgboost(self):
-        mlflow.set_tracking_uri('http://localhost:5000')
-        mlflc = MLflowCallback(metric_name="rmse",create_experiment=False)
-        study = optuna.create_study(direction="minimize")
-        study.optimize(self.objective,n_trials=10,callbacks=[mlflc])
+    # def optimize_xgboost(self):
+    #     experiment_id = create_mlflow_experiment(experiment_name="XGBoost_trail",
+    #                                              artifact_location = os.path.join(os.getcwd(), "xgboost_mlflow_artifacts"),
+    #                                              tags={"env":"dev","version":"0.0.1"})
+    #     mlflow.set_tracking_uri('http://localhost:5000')
+    #     with mlflow.start_run(experiment_id=experiment_id):
+        
+    #         # mlflc = MLflowCallback(metric_name="rmse")
+    #         study = optuna.create_study(direction="minimize")
+    #     #     study.optimize(lambda trial: self.objective(trial, experiment_id),n_trials=10,callbacks=[mlflc])
         # print('Number of finished trials:', len(study.trials))
         # print('Best trial:')
         # trial = study.best_trial
@@ -66,6 +79,13 @@ class ModelTrainer:
         # print('Params: ')
         # for key, value in trial.params.items():
         #     print(f'{}: {}'.format(key, value))
+    def optimize_xgboost(self):
+        mlflow.set_tracking_uri('http://localhost:5000')          
+        study = optuna.create_study(direction="minimize")
+        mlflc = MLflowCallback(metric_name="rmse")
+        study.optimize(self.objective,n_trials=10,callbacks=[mlflc])
+
+
 
         
 
--- src/utils/common.py
+++ src/utils/common.py
@@ -8,6 +8,7 @@ from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
+import mlflow
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
@@ -94,6 +95,45 @@ def get_size(path: Path) -> str:
     size_in_kb = round(os.path.getsize(path)/1024)
     return f"~ {size_in_kb} KB"
 
+def create_mlflow_experiment(experiment_name:str,artifact_location:str, tags:dict[str, Any]) -> str:
+    """
+    create a new mlflow experiment with the given name and artifact location
+    Args:
+        experiment_name (str): The name of the experiment to create
+        artifact_location (str): The artifact location of the experiment to create.
+        tags (dict[str,Any]): The tags of the experiment to create.
+    
+    Returns:
+        experiment_id (str): The id of the created experiment.
+    """
+    try:
+        experiment_id = mlflow.create_experiment(name=experiment_name,artifact_location=artifact_location,tags=tags)
+    except:
+        print(f"Experiment {experiment_name} already exists.")
+        experiment_id = mlflow.get_experiment_by_name(experiment_name).experiment_id
 
+    mlflow.set_experiment(experiment_name=experiment_name)
+
+    return experiment_id
+
+@ensure_annotations
+def get_mlflow_experiment( experiment_id: str = None, experiment_name:str = None) -> mlflow.entities.Experiment:
+    """
+    Retrieve the mlflow experiment with the given id or name
+    Args:
+        experiment_id(str): The id of the experiment to retrieve
+        experiment_name(str): The name of the experiment to retrieve.
+
+    Returns:
+        experiment(mlflow.entities.Experiment): The mlflow experiment with the given id or name 
+    """
+    if experiment_id is not None:
+        experiment = mlflow.get_experiment(experiment_id)
+    elif experiment_name is not None:
+        experiment = mlflow.get_experiment_by_name(experiment_name)
+    else:
+        raise ValueError("Either experiment_id or experiment_name must be provided.")
+    return experiment
+    
 
 
